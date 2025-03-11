88a3ccf4866ff93863794c421a52c3689d63c35e
Srikaran Devarakonda <srikaran2230@gmail.com>
2025-03-11 11:06:53 +0530

    created /generate command

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ import userModel from './src/models/User.js';
 import eventModel from './src/models/Event.js';
 import connectDB from './src/config/db.js';
 import {message} from 'telegraf/filters';
+import OpenAI from 'openai';
+import { captureRejectionSymbol } from 'events';
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const client = new OpenAI({
+    apiKey: process.env['OPENAI_KEY'], // This is the default and can be omitted
+  });
 
 
 try{
@@ -47,6 +53,46 @@ bot.start(async(ctx)=>{
 
 });
 
+bot.command('generate',async(ctx)=>{
+    //get events for the user
+
+    const startOfDay = new Date();
+    startOfDay.setHours(0,0,0,0);
+
+    const endOfTheDay = new Date();
+    endOfTheDay.setHours(23,59,59,999);
+
+    const events = await eventModel.find({tgId:ctx.update.message.from.id,
+        createdAt:{
+            $gte:startOfDay,
+            $lte:endOfTheDay,
+        },
+    });
+    if(events.length===0){
+        await ctx.reply("No events for the day.");
+        return ;
+    }
+    console.log('events',events);
+
+    //make openai api call 
+     try{
+        const chatCompletion = await client.chat.completions.create({
+            messages: [
+                {role: "system", content: `Act as a senior copywriter, you write highly engaging posts for Linkedin, facbook and twitter using provided thoughts/events through out the day.`},
+                {role: "user", content: `Write like a human, for humans. Craft three engaging social media posts tailored for Linkedin, Facebook and Twitter Audiences.Use simple language. Use given time labels just to understand the order of the events, don't mention the time in the posts. Each post should creatively highlight the following events. Ensure the tone is conversational and impactful. Focus on engaging the respective platforms audience,encouraging Interaction and driving interest in the events: ${events.map((event) => event.text).join(', ')}`}
+            ],
+            model:process.env.OPENAI_MODEL,
+        });
+     }catch(err){
+
+     }
+    
+    //store token count
+    //send response to the user
+    await ctx.reply('Generating posts...');
+
+});
+
 bot.on(message('text'),async(ctx)=>{
     const from = ctx.update.message.from;
     const message = ctx.update.message.text;
@@ -63,6 +109,8 @@ bot.on(message('text'),async(ctx)=>{
     }
 
 });
+
+
 
 bot.launch();
 
